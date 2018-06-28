0fca418a2ff15e47f61cc7a9cac14396365495ed
pratiks14 <pratikshetty14@gmail.com>
2018-06-28 17:18:08 +0530

    Update bank.sql

--- bank.sql
+++ bank.sql
@@ -1,1 +1,13 @@
 
+create table netbankusers(
+customerid char(7) primary key,
+emailid varchar(25) not null,
+mobileno char(10) not null,
+name varchar(25) not null,
+password varchar(25) not null,
+login_time datetime2
+);
+
+drop table netbankusers;
+select * from netbankusers;
+select login_time from netbankusers where customerid ='1141621'
