4b5618de1c001a242c801191e35846839d7c91ea
pratiks14 <pratikshetty14@gmail.com>
2018-06-29 16:12:15 +0530

    Update bank.sql

--- bank.sql
+++ bank.sql
@@ -1,4 +1,3 @@
-
 create table netbankusers(
 customerid char(7) primary key,
 emailid varchar(25) not null,
@@ -11,3 +10,17 @@ login_time datetime2
 drop table netbankusers;
 select * from netbankusers;
 select login_time from netbankusers where customerid ='1141621'
+
+create table accountdetails(
+accountno char(16) primary key,
+customerid char(7) references netbankusers(customerid) on delete cascade,
+acc_balance decimal(40,4),
+acc_type varchar(15),
+mail_address varchar(100),
+branch_name varchar(25) not null,
+branch_code varchar(10) ,
+created_date datetime2
+
+
+
+);
