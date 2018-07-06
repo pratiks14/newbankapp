83199957d307ea634d88bdb972a9227b81982fbc
pratiks14 <pratikshetty14@gmail.com>
2018-07-06 16:16:07 +0530

    Update bank.sql

--- bank.sql
+++ bank.sql
@@ -14,13 +14,34 @@ select login_time from netbankusers where customerid ='1141621'
 create table accountdetails(
 accountno char(16) primary key,
 customerid char(7) references netbankusers(customerid) on delete cascade,
-acc_balance decimal(40,4),
+acc_balance decimal(20,2) default(10000.0),
 acc_type varchar(15),
-mail_address varchar(100),
+mail_address varchar(250),
 branch_name varchar(25) not null,
 branch_code varchar(10) ,
-created_date datetime2
-
-
-
+created_date datetime2 ,
+debitcardno char(16),
+aadhar_no char(12),
+status varchar(20) default('Applied')
 );
+
+drop table accountdetails;
+select * from accountdetails;
+delete from accountdetails;
+
+create table depositdetails(
+depositno char(10) primary key,
+customerid char(7) references netbankusers(customerid),
+deposit_amount decimal(20,2),
+debit_acc char(16) references accountdetails(accountno) on delete cascade,
+recur_months  char(2),
+day_of_month char(2),
+created_on datetime2
+);
+drop table depositdetails;
+delete from depositdetails;
+select * from depositdetails;
+
+
+update accountdetails set acc_balance =acc_balance - 100.0 where accountno = '7318838545954363'
+update accountdetails set acc_balance =acc_balance - 5000.00 where accountno = '7318838545954363'
