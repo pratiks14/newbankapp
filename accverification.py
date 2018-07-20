69579b63afcaff8b7703f33cdbf038e747793b84
pratiks14 <pratikshetty14@gmail.com>
2018-07-20 16:15:43 +0530

    Update accverification.py

--- accverification.py
+++ accverification.py
@@ -27,7 +27,7 @@ class Verification():
     @staticmethod
     def validateAadhar(aadharno):
         if re.match(r'^[0-9]{12}$',aadharno) is None:
-            response = make_response(json.dumps('Invalid Aadhar Card Number'),401)
+            response = make_response(json.dumps('Invalid Aadhar Card Number'),400)
             response.headers['Content-Type'] = 'application/json'
             return response
         else:
@@ -36,7 +36,7 @@ class Verification():
     @staticmethod
     def validateAddress(address):
         if len(address) >220:
-            response = make_response(json.dumps('Your Address is too long'),401) 
+            response = make_response(json.dumps('Your Address is too long'),400) 
             response.headers['Content-Type'] = 'application/json'
             return response
         else:
@@ -45,7 +45,7 @@ class Verification():
     @staticmethod
     def validateZip(zip):
         if re.match(r'^[0-9]{6}$',zip) is None:
-            response = make_response(json.dumps('Invalid ZIP code'),401)
+            response = make_response(json.dumps('Invalid ZIP code'),400)
             response.headers['Content-Type'] = 'application/json'
             return response    
         else:
@@ -54,12 +54,12 @@ class Verification():
     @staticmethod
     def verifyDebitAccount(accountno,amount):
         if re.match(r'^\d+$',amount) is None:
-            response = make_response(json.dumps('Amount should be a number.'),401)
+            response = make_response(json.dumps('Amount should be a number.'),400)
             response.headers['Content-Type'] = 'application/json'
             return response
 
         if int(amount) % 500 != 0 or int(amount) / 500 == 0:
-            response = make_response(json.dumps('Min Amount is Rs 500 or multiple of 500'),401)
+            response = make_response(json.dumps('Min Amount is Rs 500 or multiple of 500'),400)
             response.headers['Content-Type'] = 'application/json'
             return response
 
@@ -69,13 +69,13 @@ class Verification():
         cursor.execute(sql)
         account = cursor.fetchone()
         if account is not None:
-            response = make_response(json.dumps('This account has already been linked to a Deposit Account'),401)
+            response = make_response(json.dumps('This account has already been linked to a Deposit Account'),400)
             response.headers['Content-Type'] = 'application/json'
             return response
 
         acc_balance = dbase.getAccountBalance(accountno)
         if float(acc_balance) < float(amount):
-            response = make_response(json.dumps('Not enough balance in your acccount.'),401)
+            response = make_response(json.dumps('Not enough balance in your acccount.'),400)
             response.headers['Content-Type'] = 'application/json'
             return response   
         
@@ -88,7 +88,7 @@ class Verification():
     @staticmethod
     def verifyAccountExists(accountno,live=False):
         if re.match(r'^\d{16}$',accountno) is None:
-            response = make_response(json.dumps('Enter 16 digit account Number'),401)
+            response = make_response(json.dumps('Enter 16 digit account Number'),400)
             response.headers['Content-Type'] = 'application/json'
             return response
         db = dbase.getDB()
@@ -99,7 +99,7 @@ class Verification():
         cursor.execute(sql)
         account = cursor.fetchall()
         if account == []:
-            response = make_response(json.dumps('There is no Account with this Account Number!'),401)
+            response = make_response(json.dumps('There is no Account with this Account Number!'),400)
             response.headers['Content-Type'] = 'application/json'
             return response
         return None    
@@ -107,12 +107,12 @@ class Verification():
     @staticmethod
     def validatePin(pin):
         if re.match(r'^\d+$',pin) is None:
-            response = make_response(json.dumps('PIN should be digits'),401)
+            response = make_response(json.dumps('PIN should be digits'),400)
             response.headers['Content-Type'] = 'application/json'
             return response
 
         if len(pin) != 4:
-            response = make_response(json.dumps('It should 4 digit!'),401)
+            response = make_response(json.dumps('It should 4 digit!'),400)
             response.headers['Content-Type'] = 'application/json'
             return response
         
@@ -121,18 +121,18 @@ class Verification():
     @staticmethod
     def validateAmount(accountno,amount):
         if re.match(r'^\d+$',amount) is None:
-            response = make_response(json.dumps('Amount should be a number.'),401)
+            response = make_response(json.dumps('Amount should be a number.'),400)
             response.headers['Content-Type'] = 'application/json'
             return response
 
         if int(amount) % 500 != 0 or int(amount) / 500 == 0:
-            response = make_response(json.dumps('Min Amount is Rs 500 or multiple of 500'),401)
+            response = make_response(json.dumps('Min Amount is Rs 500 or multiple of 500'),400)
             response.headers['Content-Type'] = 'application/json'
             return response
         
         acc_balance = dbase.getAccountBalance(accountno)
         if float(acc_balance) < float(amount):
-            response = make_response(json.dumps('Not enough balance in your acccount.'),401)
+            response = make_response(json.dumps('Not enough balance in your acccount.'),400)
             response.headers['Content-Type'] = 'application/json'
             return response
 
