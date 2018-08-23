854f3e3e194644fb1528e49a94b8a41383222296
pratiks14 <pratikshetty14@gmail.com>
2018-08-23 17:11:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import traceback
 from flask import make_response,Response
 from flask_restful import Resource,Api
 from model import Validation,Operations
+import time
 
 app = Flask(__name__)
 app.secret_key = "my_app_secretkey"
@@ -68,12 +69,6 @@ def login():
 
 
 	return render_template('login.html')
-
-@app.route('/disconnect')
-def disconnect():
-	login_session.clear()
-	return redirect('/')
-
 @app.route('/main')
 def rendermain():
 	if "username" not in login_session:
@@ -82,11 +77,94 @@ def rendermain():
 	try:
 		customerDetailsDict = Operations.getCustomerDetailsDict(login_session['ssn'])
 		customerDetailsDict['error'] = 'No'
+
 	except Exception as e:
 		print(e)
 		traceback.print_exc()
 		customerDetailsDict['error'] = 'error'
 	return render_template('index.html',customerDetails = customerDetailsDict)
+
+@app.route('/accounts/<accounttype>/<accountno>',methods=['GET'])
+def accounts(accounttype,accountno):
+	if "username" not in login_session:
+		return redirect('/')
+	try:
+		if request.method == 'GET':
+			detailsDict = Operations.getAccountDetails(accounttype,accountno)
+			# response = make_response(json.dumps(detailsDict), 400)
+			# response.headers['Content-Type'] = 'application/json'
+			return render_template('cardDetails.html',details = detailsDict)
+	except Exception as e:
+		print(e)
+		traceback.print_exc()
+		response = make_response(json.dumps('Some Error Occured! Refresh and Try again '), 400)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
+@app.route('/accounts/<accounttype>/<accountno>/transactions',methods=['GET'])
+def transactions(accounttype,accountno):
+	try:
+		data = request.args
+		startdate = time.strptime(data['startdate'],'%m/%d/%Y')
+		enddate = time.strptime(data['enddate'],'%m/%d/%Y')
+		transactionDict = Operations.getTransactions(accounttype,accountno,startdate,enddate)
+		response = make_response(json.dumps(transactionDict), 200)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+	except ValueError:
+		response = make_response(json.dumps('Invalid Date Input '), 400)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+	except Exception as e:
+		traceback.print_exc()
+		response = make_response(json.dumps('Some Error Occured! Refresh and Try again '), 400)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+	
+@app.route('/payments')
+def payments():
+	if "username" not in login_session:
+		return redirect('/')
+	return render_template('payments.html')
+
+@app.route('/moneytransfer',methods=['GET','POST'])
+def moneytransfer():
+	if request.method== 'GET':
+		if "username" not in login_session:
+			return redirect('/')
+		accounts = Operations.getSavingsAccounts()
+		
+		return render_template('moneytransfer.html',accounts = accounts)
+	else:
+		try:
+			params = request.form
+			resp = Operations.moneyTransferValidate(params)
+			if resp != None:
+				return resp
+			Operations.transferAmount(params)
+			response = make_response(json.dumps('Transfer Completed'), 200)
+			response.headers['Content-Type'] = 'application/json'
+			return response	
+		except Exception as e:
+			traceback.print_exc()
+			response = make_response(json.dumps('Some Error Occured! Refresh and Try again '), 400)
+			response.headers['Content-Type'] = 'application/json'
+			return response	
+
+
+@app.route('/payment/<accounttype>/<number>')
+def payment(accounttype,number):
+	accounts = Operations.getSavingsAccounts()
+	return render_template('payments.html',accounts=accounts)
+
+
+@app.route('/disconnect')
+def disconnect():
+	login_session.clear()
+	return redirect('/')
+
+
+
 
 
 if __name__ == '__main__':
